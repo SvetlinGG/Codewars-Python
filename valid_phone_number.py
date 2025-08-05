14b00972235aefe9c6c20f3edd611218bb4c6fb7
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-05 11:22:38 +0200

    valid phone number

--- valid_phone_number.py
+++ valid_phone_number.py
@@ -1,15 +1,11 @@
 
 import re
 def valid_phone_number(phone_number):
+    #pattern = r'\(\d{3}\) \d{3}-\d{4}'
     pattern = r'[(]123[)]\s([0-9]{3})-([0-9]{4})'
-    matches = re.findall(pattern, phone_number)
+    return bool(re.fullmatch(pattern, phone_number))
 
-    if matches == phone_number:
-        return True
-    else:
-        return False
-
-result = valid_phone_number('(123) 456-7890')
+#result = valid_phone_number('(123) 456-7890')
 #result = valid_phone_number('(1111)555 2345')
-#result = valid_phone_number('(098) 123 4567')
+result = valid_phone_number('(098) 123 4567')
 print(result)
