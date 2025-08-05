e7218af2039e449743ce40a4bfb3df8903375abe
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-05 14:16:54 +0200

    counting duplicates

--- counting_duplicates.py
+++ counting_duplicates.py
@@ -1,18 +1,16 @@
-from itertools import count
-
 
 def duplicate_count(text):
+    letters = {}
+    for char in text:
+        letters[char] = letters.get(char, 0) + 1
+        if letters[char] > 0:
+            letters[count] = letters.get(count, 0) + 1
 
 
-
-    for item in text:
-        letters = {'char': '', 'count': 0}
-
-        if letters['count'] == 0:
-            letters['char'] += item
-            letters['count'] += 1
+        if letters[count] == 0:
             return 'no characters repeats more than once'
-        elif letters['count'] > 0:
-            return f"{letters['char']} and {letters['char']}"
-result = duplicate_count("abcde")
+        elif letters[count] > 0:
+            return f"{letters[char]} and {letters[char]}"
+#result = duplicate_count("abcde")
+#result = duplicate_count("aabbcde" )
 print(result)
